8126560ae66769fa821d3d00290e5a1a453702cf
hiiragi123-tec <hirose.shuuta@cti.co.jp>
2025-03-13 20:24:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Chuden CTI!!_hiiragi');
+  res.end('Hello Chuden CTI!!_hiiragi_v2');
 });
 
 server.listen(8080, () => {
